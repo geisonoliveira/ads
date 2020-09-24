a0ad982ba959a8b7cb83177dae3e86902dff4dd4
Geison <gs_gaara@hotmail.com>
2020-09-24 20:19:22 -0300

    modificação

--- login teste.py
+++ login teste.py
@@ -2,5 +2,7 @@ usuario = input("Digite seu usuario:")
 senha = input("Digite sua senha:")
 if usuario == "geison" and senha == "1234":
     print("Geison, você foi logado com sucesso!")
+elif usuario == "katia" and senha == "4321":
+    print("Kátia, você foi logada com sucesso!")    
 else:
     print("Senha incorreta")
