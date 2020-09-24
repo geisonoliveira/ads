e97b567600ce52b0c7d4360d6593d3816f1575cc
Geison <gs_gaara@hotmail.com>
2020-09-24 20:08:53 -0300

    primeiro teste

---
+++ teste.py
@@ -0,0 +1,6 @@
+usuario = input("Digite seu usuario:")
+senha = input("Digite sua senha:")
+if usuario == "geison" and senha == "1234":
+    print("Geison, você foi logado com sucesso!")
+else:
+    print("Senha incorreta")
